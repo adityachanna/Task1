45daf91afd44da7398c537422d37ae94b13b7479
adiyachanna <153483429+adiyachanna@users.noreply.github.com>
2024-10-29 17:32:41 +0530

    Update Micrograd.py

--- Micrograd.py
+++ Micrograd.py
@@ -1,43 +1,49 @@
+
 class Value:
-    
-    def __init__(self, data, _child=(), _op='', label=''):
+
+    def __init__(self, data, _children=(), _op=''):
         self.data = data
-        self._prev = set(_child)
-        self._op = _op
-        self.label = label
-        self.grad = 0.0
+        self.grad = 0
+        self._prev = set(_children)
         self._backward = lambda: None
-        
-    def __repr__(self):
-        return f"Value(data={self.data})"
-    
     def __add__(self, other):
         out = Value(self.data + other.data, (self, other), '+')
-        
         def _backward():
-            self.grad += 1.0 * out.grad
-            other.grad += 1.0 * out.grad
+            self.grad += out.grad
+            other.grad += out.grad
         out._backward = _backward
-        
-        return out 
-    
+        return out
     def __mul__(self, other):
         out = Value(self.data * other.data, (self, other), '*')
-                
         def _backward():
             self.grad += other.data * out.grad
             other.grad += self.data * out.grad
         out._backward = _backward
-        
         return out
-    
-    def tanh(self):
-        x = self.data
-        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
-        out = Value(t, (self, ), 'tanh')
-        
+    def __pow__(self, other):
+        out = Value(self.data**other, (self,), f'**{other}')
         def _backward():
-            self.grad += (1 - t**2) * out.grad
+            self.grad += (other * self.data**(other-1)) * out.grad
         out._backward = _backward
-        
         return out
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+        def _backward():
+            self.grad += (out.data > 0) * out.grad
+        out._backward = _backward
+        return out
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
+    def __repr__(self):
+        return f"Value(data={self.data}, grad={self.grad})"
