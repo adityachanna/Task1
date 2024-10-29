ce986060dd9ce0fb36ec7feb4c60558a7613bdba
adiyachanna <153483429+adiyachanna@users.noreply.github.com>
2024-10-29 17:22:29 +0530

    Update Micrograd.py

--- Micrograd.py
+++ Micrograd.py
@@ -1,1 +1,43 @@
-
+class Value:
+    
+    def __init__(self, data, _child=(), _op='', label=''):
+        self.data = data
+        self._prev = set(_child)
+        self._op = _op
+        self.label = label
+        self.grad = 0.0
+        self._backward = lambda: None
+        
+    def __repr__(self):
+        return f"Value(data={self.data})"
+    
+    def __add__(self, other):
+        out = Value(self.data + other.data, (self, other), '+')
+        
+        def _backward():
+            self.grad += 1.0 * out.grad
+            other.grad += 1.0 * out.grad
+        out._backward = _backward
+        
+        return out 
+    
+    def __mul__(self, other):
+        out = Value(self.data * other.data, (self, other), '*')
+                
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        
+        return out
+    
+    def tanh(self):
+        x = self.data
+        t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+        out = Value(t, (self, ), 'tanh')
+        
+        def _backward():
+            self.grad += (1 - t**2) * out.grad
+        out._backward = _backward
+        
+        return out
